13bc87d67ccaf760dcd51f31bfb399597d848e13
elia1986 <caponielia@gmail.com>
2026-01-30 14:26:51 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
 
@@ -9,6 +8,7 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
+        // Versioni stabili per evitare conflitti con Gradle 9
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
@@ -23,8 +23,12 @@ allprojects {
     }
 }
 
+// Queste funzioni servono a Gradle per riconoscere i blocchi 'cloudstream' e 'android'
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = 
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+    (extensions.getByName("cloudstream") as CloudstreamExtension).configuration()
+
+fun Project.android(configuration: BaseExtension.() -> Unit) = 
+    (extensions.getByName("android") as BaseExtension).configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
@@ -50,10 +54,11 @@ subprojects {
     }
 
     dependencies {
+        val implementation by configurations
         val cloudstream by configurations
+        
         cloudstream("com.lagradost:cloudstream3:pre-release")
         
-        val implementation by configurations
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.15.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
