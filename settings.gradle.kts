0c78505bc3b7877923661e273be615e6fb2486c9
elia1986 <caponielia@gmail.com>
2026-02-01 20:49:19 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,7 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
+// Includiamo esplicitamente solo la tua cartella
+include(":Chatrubate")
 
-val disabled = listOf<String>()
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
-
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+// Se hai ancora la cartella ExampleProvider e vuoi essere sicuro che venga ignorata:
+// project(":ExampleProvider").projectDir = file("non-existent")
