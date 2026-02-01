1b2bd059639f940855682a5f8c374dc37cdfda0d
elia1986 <caponielia@gmail.com>
2026-02-01 21:43:31 +0100

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "CloudstreamPlugins"
 
 // Includiamo esplicitamente solo la tua cartella
-include(":Chatrubate")
+include(":Chaturbate")
 
 // Se hai ancora la cartella ExampleProvider e vuoi essere sicuro che venga ignorata:
 // project(":ExampleProvider").projectDir = file("non-existent")
